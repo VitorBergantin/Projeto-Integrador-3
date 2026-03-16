9508a1e2dc9e2eed6a01ad12f6184f609353cd3a
VitorBergantin <vitor.br@puccampinas.edu.br>
2026-03-15 21:40:57 -0300

    Base do projeto

---
+++ android/app/src/main/kotlin/com/example/projeto_integrador_3/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.projeto_integrador_3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/controllers/pontos_controller.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class PontosController extends ChangeNotifier {
+  double lati = 0.0;
+  double long = 0.0;
+  String erro = '';
+
+  PontosController() {
+    getPosicao();
+  }
+
+  Future<void> getPosicao() async {
+    try {
+      Position posicao = await _posicaoAtual();
+      lati = posicao.latitude;
+      long = posicao.longitude;
+    } catch (e) {
+      erro = e.toString();
+    }
+    notifyListeners();
+  }
+
+  Future<Position> _posicaoAtual() async {
+    LocationPermission permissao;
+
+    bool ativado = await Geolocator.isLocationServiceEnabled();
+
+    if (!ativado) {
+      return Future.error('Por favor, habilite a localização');
+    }
+
+    permissao = await Geolocator.checkPermission();
+    
+    if (permissao == LocationPermission.denied) {
+      permissao = await Geolocator.requestPermission();
+
+      if (permissao == LocationPermission.denied) {
+        return Future.error('Precisamos que autorize acesso a localização');
+      }
+    }
+
+    if (permissao == LocationPermission.deniedForever) {
+      return Future.error('Por favor, habilite a localização');
+    }
+
+    return await Geolocator.getCurrentPosition();
+  }
+}
